338d588448e3faf091931060f176daa2c34d85d2
Rajith Jarang <rajithjarang@gmail.com>
2022-12-16 10:28:51 +0530

    removed all the files from gitignore

--- lib/constants/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constants {
-  Constants._();
-
-  static const double neumorphicDepth = 8;
-  static const double neumorphicIntensity = 1;
-  static const double neumorphicSurfaceIntensity = 10;
-}
--- lib/data/localdb/constants/db_constants.dart
+++
@@ -1,6 +0,0 @@
-class DBConstants {
-  DBConstants._();
-
-  static const String STORE_NAME = "store";
-  static const String DB_NAME = "db";
-}
