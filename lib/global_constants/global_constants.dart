bbd450c69bbc9888be7b6f538c30c70dd4751fc5
Rajith Jarang <rajithjarang@gmail.com>
2023-04-26 09:28:52 +0530

    added a domain

--- lib/global_constants/global_constants.dart
+++ lib/global_constants/global_constants.dart
@@ -31,7 +31,8 @@ class GlobalConstants {
   static const int adBannerDuration = 15;
   // static const String appUrl =
   //     "https://core.cybernate-retail.erstnot.in/graphql";
-  static const String appUrl = "http://localhost:8000/graphql/";
+  static const String appUrl =
+      "https://dev.cybernate-retail.core.erstnot.in/graphql/";
   // static const String appUrl =
   //     "https://9996-2406-b400-b4-1f03-2589-cf0b-61fe-ffe9.ngrok-free.app/graphql/";
   static const String defaultChannel = "default-channel";
