efe0cc37e9bc4eefa6a95872fc98a7ca6aa9c226
Rajith Jarang <rajithjarang@gmail.com>
2023-07-02 21:27:50 +0530

    removed comment

--- lib/global_constants/global_constants.dart
+++ lib/global_constants/global_constants.dart
@@ -33,8 +33,7 @@ class GlobalConstants {
   //     "https://core.cybernate-retail.erstnot.in/graphql";
   static const String appUrl =
       "https://dev.cybernate-retail.core.erstnot.in/graphql/";
-  // static const String appUrl =
-  //     "https://9996-2406-b400-b4-1f03-2589-cf0b-61fe-ffe9.ngrok-free.app/graphql/";
+
   static const String defaultChannel = "default-channel";
   static const defaultLanguage = GLanguageCodeEnum.TE_IN;
   static const LatLng currentCountryLocation = LatLng(20.5937, 78.9629);
