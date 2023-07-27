202df161964200f21e39095fe814331de0671b92
netfreaker <rahulkavati0406@gmail.com>
2023-07-27 23:46:10 +0530

    added email to Chat Us View

--- lib/ui/screens/contact_us/contact_us_chat.dart
+++ lib/ui/screens/contact_us/contact_us_chat.dart
@@ -13,6 +13,30 @@ class _ContactUsChatState extends State<ContactUsChat> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: justBackButtonWithTitleAppBar(context, "Customer support"),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Email:",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: Theme.of(context).textTheme.bodyMedium?.fontSize,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              "cyberself@erstnot.in",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: Theme.of(context).textTheme.bodyLarge!.fontSize,
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
